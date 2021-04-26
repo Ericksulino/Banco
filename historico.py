8b72846af48670b8a528e1c58dc3a1becd2b52ad
Ericksulino Moura <62266038+Ericksulino@users.noreply.github.com>
2021-04-26 10:35:01 -0300

    classe historico

---
+++ historico.py
@@ -0,0 +1,16 @@
+import datetime
+
+class Historico:
+
+    def __init__(self):
+        self._data_abertura = datetime.datetime.today()
+        self._transacoes = []
+
+    def adicionar_transacao(self, str):
+        self._transacoes.append(str)
+
+    def imprimir_transacoes(self):
+        print("Conta aberta em: ", self._data_abertura)
+        print("Transações:")
+        for t in self._transacoes:
+            print(t)
