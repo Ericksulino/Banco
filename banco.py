bb742c7e580f2ca522f0008f7870c05120580d68
Ericksulino Moura <62266038+Ericksulino@users.noreply.github.com>
2021-05-09 20:13:13 -0300

    criando classe Banco

---
+++ banco.py
@@ -0,0 +1,48 @@
+from cliente import Cliente
+from conta import Conta
+
+class Banco:
+    def __init__(self):
+        self._lista_clie = {}
+        self._lista_contas = {}
+    
+    def busca_conta(self,num):
+        if num in self._lista_contas:
+            return self._lista_contas[num]
+        else:
+            return None
+    
+    def busca_clie(self,cpf):
+        if cpf in self._lista_clie:
+            return self._lista_clie[cpf]
+        else:
+            return None
+
+    def adcionar_conta(self,cnta):
+        if self.busca_conta(cnta.num) != None:
+            return False
+        else:
+            self._lista_contas[cnta.numero] = cnta
+            return True
+    
+    def adicionar_cliente(self,clie):
+        if self.busca_clie(clie.cpf) != None:
+            return False
+        else:
+            self._lista_contas[clie.cpf] = clie
+            return True
+
+    def transfere(self,num,numDest,valor):
+        if self.busca_conta(num) != None:
+            if self.busca_conta(numDest) != None:
+                if (self._lista_contas[num].transfere(valor,self._lista_contas[numDest])):
+                    return True
+                else:
+                    return False
+            else: 
+                return False
+        else:
+            return False
+    
+                
+        
