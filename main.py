0f160a78f03d45373a27eec721a9bd02eadc7bc3
Ericksulino Moura <62266038+Ericksulino@users.noreply.github.com>
2021-05-10 14:41:11 -0300

    alterações nas telas

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ from tela_extrato import Tela_extrato
 from tela_saque import Tela_saque
 from tela_transferen import Tela_transferen
 from cadastroCliente import Tela_Cadastro_Cliente
+from tela_menu_conta import Tela_Menu_Conta
+from tela_login import Tela_Login
 
 class Ui_Main(QtWidgets.QWidget):
     def setupUi(self, Main):
@@ -30,6 +32,9 @@ class Ui_Main(QtWidgets.QWidget):
         self.stack3 = QtWidgets.QMainWindow()
         self.stack4 = QtWidgets.QMainWindow()
         self.stack5 = QtWidgets.QMainWindow()
+        self.stack6 = QtWidgets.QMainWindow()
+        self.stack7 = QtWidgets.QMainWindow()
+        self.stack8 = QtWidgets.QMainWindow()
 
         self.tela_inicio = Tela_Home()
         self.tela_inicio.setupUi(self.stack0)
@@ -49,21 +54,40 @@ class Ui_Main(QtWidgets.QWidget):
         self.tela_transf = Tela_transferen()
         self.tela_transf.setupUi(self.stack5)
 
+        self.tela_menu_clie = Menu_Cliente()
+        self.tela_menu_clie.setupUi(self.stack6)
+
+        self.tela_login = Tela_Login()
+        self.tela_login.setupUi(self.stack7)
+
+        self.tela_menu_conta = Tela_Menu_Conta()
+        self.tela_menu_conta.setupUi(self.stack8)
+
         self.QtStack.addWidget(self.stack0)
         self.QtStack.addWidget(self.stack1)
         self.QtStack.addWidget(self.stack2)
         self.QtStack.addWidget(self.stack3)
         self.QtStack.addWidget(self.stack4)
         self.QtStack.addWidget(self.stack5)
+        self.QtStack.addWidget(self.stack6)
+        self.QtStack.addWidget(self.stack7)
+        self.QtStack.addWidget(self.stack8)
+
 class Main(QMainWindow,Ui_Main):
     def __init__(self,parent=None):
         super(Main,self).__init__(parent)
         self.setupUi(self)
 
         self.ban = Banco()
-        self.tela_inicio.ButCliente.clicked.connect(self.abrirTelaCadClie)
-        self.tela_inicio.ButConta.clicked.connect(self.abrirTelaConta)
+        self.tela_inicio.ButCliente.clicked.connect(self.abrirMenuClie)
+        self.tela_inicio.ButConta.clicked.connect(self.abrirTelaLog)
 
+        self.tela_login.ButCadas.clicked.connect(self.abrirTelaConta)
+        self.tela_login.ButHome.clicked.connect(self.abrirHome)
+
+        self.tela_menu_clie.ButCadastrar.clicked.connect(self.abrirTelaCadClie)
+        self.tela_menu_clie.ButHome.clicked.connect(self.abrirHome)
+        
         self.tela_cadsClie.ButCadastrar.clicked.connect(self.botaoCadastClie)
         self.tela_cadsClie.ButHome.clicked.connect(self.abrirHome)
     
@@ -83,7 +107,24 @@ class Main(QMainWindow,Ui_Main):
         else:
             QMessageBox.information(None,'Banco','Todos os campos devem ser preeecidos!')
         self.abrirHome()
-
+    '''
+    def botaoCadastConta(self):
+        num = self.tela_cadsClie.lineEdit.text()
+        cpf = self.tela_cadsClie.lineEdit_2.text()
+        nascimento = self.tela_cadsClie.lineEdit_3.text()
+        if not(nome == '' or cpf == '' or nascimento == ''):
+            p = Cliente(nome,cpf,nascimento)
+            if(self.ban.adicionar_cliente(p)):
+                QMessageBox.information(None,'Banco','Cadastro realizado com sucesso!')
+                self.tela_cadsClie.lineEdit.setText('')
+                self.tela_cadsClie.lineEdit_2.setText('')
+                self.tela_cadsClie.lineEdit_3.setText('')
+            else:
+                QMessageBox.information(None,'Banco','O CPF informado já se encontra cadastrado!')
+        else:
+            QMessageBox.information(None,'Banco','Todos os campos devem ser preeecidos!')
+        self.abrirHome()
+'''
     
     def abrirHome(self):
         self.QtStack.setCurrentIndex(0)
@@ -102,6 +143,15 @@ class Main(QMainWindow,Ui_Main):
     
     def abrirTelaTransf(self):
         self.QtStack.setCurrentIndex(5)
+    
+    def abrirMenuClie(self):
+        self.QtStack.setCurrentIndex(6)
+    
+    def abrirTelaLog(self):
+        self.QtStack.setCurrentIndex(7)
+    
+    def abrirMenuConta(self):
+        self.QtStack.setCurrentIndex(8)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
