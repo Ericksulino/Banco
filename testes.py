b30f80b2697fbb7a079b5aa570c73048191c7413
Ericksulino Moura <62266038+Ericksulino@users.noreply.github.com>
2021-05-10 11:22:11 -0300

    arquivo para realização de testes

---
+++ testes.py
@@ -0,0 +1,42 @@
+from conta import Conta
+from cliente import Cliente
+from banco import Banco
+def true_false(bol):
+    if bol == True:
+        print("Sucesso!")
+    else: print("Não foi possível!")
+
+ban = Banco()
+
+cl1 = Cliente('Erick',123,16/12/2000)
+ban.adicionar_cliente(cl1)
+cl2 = Cliente('Jose',321,12/10/1997)
+ban.adicionar_cliente(cl2)
+
+c1 = Conta(123,cl1,100,1000)
+ban.adcionar_conta(c1)
+c2 = Conta(432,cl2,850,1000)
+ban.adcionar_conta(c2)
+
+
+print("Depositos:")
+true_false(c1.deposita(10))
+true_false(c1.deposita(900))
+
+print("\n\nSaques:")
+true_false(c1.saca(20))
+true_false(c1.saca(100))
+
+print("\n\nTransferencias:")
+true_false(ban.transfere(c1.numero,c2.numero,50))
+true_false(c2.transfere(1000,c1))
+
+print("\n\nExtratos")
+c1.extrato()
+c2.extrato()
+
+print("\n\nHistorico")
+c1.ver_historico()
+c2.ver_historico()
+
+print(Conta.get_total_contas())
