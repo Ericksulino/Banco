6e7d29c6bc649011412f6fcd3042d1ac4ea92bdb
Ericksulino Moura <62266038+Ericksulino@users.noreply.github.com>
2021-05-16 19:07:03 -0300

    criando classe cliente

--- Cliente/cliente.py
+++ Cliente/cliente.py
@@ -1,14 +1,18 @@
 import socket
-ip = 'localhost'
-port = 8001
-addr = ((ip,port))
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client_socket.connect(addr)
-mensagem = ''
-while(mensagem!= 'sair'):
-    mensagem = input('digite uma mensagem para enviar ao servidor: ')
-    client_socket.send(mensagem.encode())
-    #print('mensagem enviada')
-    print('mensagem recebida: '+client_socket.recv(1024).decode())
+class Cliente:
+    def __init__(self):
+        self._ip = 'localhost'
+        self._port = 8001
+        self._addr = ((self._ip,self._port))
+        self._client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-client_socket.close()
+    def comunicacao(self):
+        self._client_socket.connect(self._addr)
+        mensagem = ''
+        while(mensagem!= 'sair'):
+            mensagem = input('digite uma mensagem para enviar ao servidor: ')
+            self._client_socket.send(mensagem.encode())
+            #print('mensagem enviada')
+            print('mensagem recebida: '+self._client_socket.recv(1024).decode())
+
+        self._client_socket.close()
