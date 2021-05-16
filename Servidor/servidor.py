2e1234ad850ffc96f817d71c4ecf70496b7d8167
Ericksulino Moura <62266038+Ericksulino@users.noreply.github.com>
2021-05-16 19:10:42 -0300

    criando classe servidor

--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -1,20 +1,29 @@
 import socket
-host = 'localhost'
-port = 8001
-addr = (host,port)
-serv_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-serv_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-serv_socket.bind(addr)
-serv_socket.listen(10)
-print("aguardando conexão...")
-con, cliente = serv_socket.accept()
-print("conectado")
-print("aguardando mensagem...")
-enviar = ''
-while(enviar!= 'sair'):
-    recebe = con.recv(1024)
-    print('mensagem recebida: '+ recebe.decode())
-    enviar = input('digite uma mensagem para enviar ao cliente: ')
-    con.send(enviar.encode())
+class Servidor:
+    def __init__(self):
+        self._host = 'localhost'
+        self.port = 8001
+        self.addr = (self._host,self.port)
+        self.serv_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    
+    def iniciar(self):
+        self.serv_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        self.serv_socket.bind(self.addr)
+        self.serv_socket.listen(10)
+        print("aguardando conexão...")
+        print("conectado")
+        print("aguardando mensagem...")
+        con, cliente = self.serv_socket.accept()
+        enviar = input('digite uma mensagem para enviar ao cliente: ')
+        con.send(enviar.encode())
+        enviar = ''
+        while(enviar!= 'sair'):
+            recebe = con.recv(1024)
+            print('mensagem recebida: '+ recebe.decode())
+            enviar = input('digite uma mensagem para enviar ao cliente: ')
+            con.send(enviar.encode())
+        
+        self.serv_socket.close()
 
-serv_socket.close()
+serv = Servidor()
+serv.iniciar()
