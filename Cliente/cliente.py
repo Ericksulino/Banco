1d9df725452e6671079881114f2b7d2c714b7e0a
Ericksulino Moura <62266038+Ericksulino@users.noreply.github.com>
2021-05-17 15:30:38 -0300

    conexão realizada

--- Cliente/cliente.py
+++ Cliente/cliente.py
@@ -1,24 +1,15 @@
 import socket
-class Cliente:
-    def __init__(self):
-        self._ip = 'localhost'
-        self._port = 8001
-        self._addr = ((self._ip,self._port))
-        self._client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-    def cli_sock_set(self,css):
-        self._client_socket = css
+ip = 'localhost'
+port = 8001
+addr = ((ip,port))
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client_socket.connect(addr)
+mensagem = ''
+while(mensagem!= 'sair'):
+    mensagem = input('digite uma mensagem para enviar ao servidor: ')
+    client_socket.send(mensagem.encode())
+    #print('mensagem enviada')
+    print('mensagem recebida: '+client_socket.recv(1024).decode())
 
-    def comunicacao(self):
-        self._client_socket.connect(self._addr)
-        mensagem = ''
-        while(mensagem!= 'sair'):
-            mensagem = input('digite uma mensagem para enviar ao servidor: ')
-            self._client_socket.send(mensagem.encode())
-            #print('mensagem enviada')
-            print('mensagem recebida: '+self._client_socket.recv(1024).decode())
-
-        self._client_socket.close()
-
-clie = Cliente()
-clie.comunicacao()
+client_socket.close()
