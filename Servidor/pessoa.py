270e9ed913ff4a7060f9e2a84bc8bf37a93e7b8c
Ericksulino <ericksulino@gmail.com>
2021-06-12 18:55:49 -0300

    tentando resolver erros

--- Servidor/pessoa.py
+++ Servidor/pessoa.py
@@ -34,19 +34,19 @@ class Cliente:
     @data_nascimento.setter
     def data_nascimento(self,dt):
         self._data_nascimento = dt'''
-
-    def cadast_clie(nome:str,cpf:str,data_nascimento:str,cursor):
-
-        if Cliente.busca_pessoa(cpf,cursor) == False:
-            cursor.execute("INSERT INTO pessoas(nome,cpf,data_nascimento) VALUES(?,?,?)",(nome,cpf,data_nascimento))
-            return True
-        return False
-
+    
     def busca_clie(buscar_cpf:str,cursor):
-
+    
         pessoa = list(cursor.execute("SELECT * FROM pessoas WHERE cpf = {}".format(buscar_cpf)))
         if (len(pessoa)!=0):
             return pessoa[0][0]
+        return False
+
+    def cadast_clie(nome:str,cpf:str,data_nascimento:str,cursor):
+
+        if Cliente.busca_clie(cpf,cursor)== False:
+            cursor.execute("INSERT INTO pessoas(nome,cpf,data_nascimento) VALUES(?,?,?)",(nome,cpf,data_nascimento))
+            return True
         return False
     
     """def imprimir_pessoa(self):
